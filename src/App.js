d0208298bd3c214724f59fc7be98388f1c5b98de
gouthamipeddiraju <gouthamipeddiraju@gmail.com>
2021-01-27 13:50:29 +1100

    created reusable components and added fetch call

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import Progressbar from "./components/Progressbar";
 
-const Progressbar = () => {};
+const FETCH_URL = "http://pb-api.herokuapp.com/bars";
 
 const App = () => {
-  const [data, setData] = useState({ isLoading: true, content: {} });
-  useEffect(() => {}, []);
+  const [data, setData] = useState({
+    isLoading: true,
+    content: {},
+    error: false,
+  });
+  const fetchData = async () => {
+    try {
+      let response = await fetch(FETCH_URL);
+      const result = await response.json();
+      setData({ isLoading: false, content: result, error: false });
+    } catch (err) {
+      setData({ isLoading: false, content: {}, error: true });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Progress Bars Demo</h1>
         {data?.isLoading ? (
           <p>loading, please wait...</p>
+        ) : data?.error ? (
+          <p> Something went wrong.</p>
         ) : (
           <Progressbar data={data.content} />
         )}
---
+++ src/components/Progressbar.js
@@ -0,0 +1,7 @@
+const Progressbar = ({ data }) => {
+  const { bars, buttons, limit } = data;
+  console.log(bars);
+  return <p>hello</p>;
+};
+
+export default Progressbar;
