df57e2d04a71d7b23bc328bad8b6cddacf63c2be
gouthamipeddiraju <gouthamipeddiraju@gmail.com>
2021-01-27 13:38:26 +1100

    initialised hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+const Progressbar = () => {};
+
+const App = () => {
+  const [data, setData] = useState({ isLoading: true, content: {} });
+  useEffect(() => {}, []);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Progress Bars Demo</h1>
+        {data?.isLoading ? (
+          <p>loading, please wait...</p>
+        ) : (
+          <Progressbar data={data.content} />
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
